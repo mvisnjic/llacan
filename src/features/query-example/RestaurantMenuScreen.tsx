13b615624b8442d4b2cde84b76e7a54e0970ea5b
enrico <kokotenrico@gmail.com>
2022-04-12 08:20:27 +0200

    Set Up Restaurant Menu Screen Look

--- src/features/query-example/RestaurantMenuScreen.tsx
+++ src/features/query-example/RestaurantMenuScreen.tsx
@@ -1,8 +1,7 @@
 import { observer } from "mobx-react";
 import React from "react";
-import { Image, StyleSheet } from "react-native";
+import { Image } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { Button } from "~/components/Button";
 import { Icon } from "~/components/Icon";
 import { Screen } from "~/components/Screen";
 import { Spacer } from "~/components/Spacer";
@@ -13,90 +12,36 @@ import { useInfiniteQuery } from "~/hooks/useInfiniteQuery";
 import { PersonInstance } from "~/mobx/entities/person/Person";
 import { useStore } from "~/mobx/utils/useStore";
 import { styleConstants as C } from "~/style/styleConstants";
-import { shadow } from "~/utils/shadow";
 
 interface PersonListItemProps {
   person: PersonInstance;
 }
 
-const allOrdersAreActive = true;
-
-function useStyle() {
-  return StyleSheet.create({
-    container: {
-      backgroundColor: allOrdersAreActive
-        ? C.colorBackgroundThemeSofter
-        : C.colorBackgroundLight,
-      ...shadow(5),
-    },
-  });
-}
-
 const PersonListItem = observer(function PersonListItem({
   person,
 }: PersonListItemProps) {
-  const S = useStyle();
-
   return (
-    <View paddingHorizontalMedium paddingVerticalSmall>
-      <View paddingLarge style={S.container}>
-        <Spacer />
-        <Text sizeExtraLarge weightBold>
+    <View
+      paddingHorizontalLarge
+      paddingVerticalSmall
+      flexDirectionRow
+      style={{
+        borderBottomWidth: 1,
+        borderBottomColor: "#EEEEEE",
+        marginBottom: -10,
+      }}
+    >
+      <View>
+        <Text sizeMedium weightSemiBold>
           {person.name}
         </Text>
-        <Spacer small />
-        <Text sizeSmall weightLight>
-          {person.gender}, {person.height}, {person.mass}
+        <Text sizeExtraSmall>
+          {person.eye_color}, {person.gender}, {person.hair_color},{" "}
+          {person.skin_color}
         </Text>
-        <Spacer />
-
-        <View>
-          <View flexDirectionRow alignItemsCenter>
-            <Icon
-              size={C.fontSizeLarge}
-              name="home-outline"
-              color={C.colorBackgroundDark}
-            />
-            <Spacer large />
-            <Text sizeMedium weightLight>
-              {person.hair_color}, {person.skin_color}, {person.eye_color}
-            </Text>
-          </View>
-          <View flexDirectionRow alignItemsCenter>
-            <Icon
-              size={C.fontSizeLarge}
-              name="phone-outline"
-              color={C.colorBackgroundDark}
-            />
-            <Spacer large />
-            <Text sizeMedium weightLight>
-              {person.birth_year}
-            </Text>
-          </View>
-          <View flexDirectionRow alignItemsCenter>
-            <Icon
-              size={C.fontSizeLarge}
-              name="circle-outline"
-              color={allOrdersAreActive ? "#2DCB48" : C.colorBackgroundLight}
-            />
-            <Spacer large />
-            <Text
-              sizeMedium
-              weightLight
-              style={{
-                color: allOrdersAreActive
-                  ? C.colorBackgroundDark
-                  : C.colorBackgroundLight,
-              }}
-            >
-              Aktivna narudžba
-            </Text>
-          </View>
-        </View>
-        <Spacer large />
-        <Button outline title="Menu i info" />
-        <Spacer />
-        <Button title={allOrdersAreActive ? "Pridruži se" : "Nova narudžba"} />
+      </View>
+      <View style={{ flex: 1, alignItems: "flex-end" }} centerContent>
+        <Text>{person.height},00</Text>
       </View>
     </View>
   );
@@ -181,6 +126,29 @@ const FlatlistHeader = observer(() => {
           <Text weightSemiBold>POMMES</Text>
         </View>
       </View>
+      <View
+        paddingHorizontalLarge
+        paddingVerticalMedium
+        style={{ borderBottomWidth: 1, borderBottomColor: "#EEEEEE" }}
+        flexDirectionRow
+        alignItemsCenter
+      >
+        <Text sizeLarge weightBold>
+          Burger
+        </Text>
+        <View
+          style={{
+            flex: 1,
+            alignItems: "flex-end",
+          }}
+        >
+          <Icon
+            size={C.fontSizeLarge}
+            name="menu-down-outline"
+            color={C.colorBackgroundDark}
+          />
+        </View>
+      </View>
     </>
   );
 });
@@ -195,10 +163,12 @@ export const RestaurantMenuScreen = observer(function QueryExample() {
 
   return (
     <Screen preventScroll>
-      <View>
+      <View style={{ paddingBottom: 10 }}>
         <StandardFlatList
           query={query}
-          contentContainerStyle={{ paddingBottom: insets.bottom }}
+          contentContainerStyle={{
+            paddingBottom: insets.bottom,
+          }}
           keyExtractor={(person) => String(person)}
           renderItem={({ item }) => <PersonListItem person={item} />}
           ListHeaderComponent={FlatlistHeader}
