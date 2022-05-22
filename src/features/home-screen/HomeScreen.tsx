e86750fbf1427ba19839db80045e77506bcd1be5
Masimo Orbanic <masimo@lloyds-digital.com>
2022-05-22 18:31:00 +0200

    review

--- src/features/home-screen/HomeScreen.tsx
+++ src/features/home-screen/HomeScreen.tsx
@@ -14,18 +14,34 @@ import { Text } from "~/components/Text";
 import { TextInput } from "~/components/TextInputFormikYup";
 import { View } from "~/components/View";
 
-const now = new Date();
-const currentYear = now.getFullYear();
-const currentMonth = now.getMonth();
-const currentDay = now.getDate();
-
 const checkFormSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email").required("Required"),
   fullName: Yup.string()
     .min(3, "Too short!")
     .max(50, "Too long!")
     .required("Required"),
-  dateOfBirth: Yup.string().required("Min years 12."),
+  dateOfBirth: Yup.string().test(
+    "is-older-than-12",
+    "Min years 12.",
+    async (value, context) => {
+      /**
+        BITNO je znat da postoji i ovaj context i ako ga console.logas 
+        vidjet ces da unutra se nalaze vrijednosti od ostalih fieldova.
+        Tako na primjer možes npr. napravit logiku da ako ti je full name 
+        "Ante" da nije bitno koliko imas godina
+      */
+
+      // console.log({context});
+      const fullName = context.parent.fullName;
+      // ako je fullName "Ante" onda ovaj yup.test vraca true
+      if (fullName === "Ante") {
+        return true;
+      }
+
+      const dayJsDate = dayjs(value);
+      return dayJsDate.isBefore(dayjs().subtract(12, "years"));
+    }
+  ),
 });
 
 export const HomeScreen = observer(function HomeScreen() {
@@ -38,7 +54,7 @@ export const HomeScreen = observer(function HomeScreen() {
           initialValues={{
             email: "",
             fullName: "",
-            dateOfBirth: "",
+            dateOfBirth: undefined,
             sex: "",
           }}
           validationSchema={checkFormSchema}
@@ -83,12 +99,14 @@ export const HomeScreen = observer(function HomeScreen() {
                   >
                     <Pressable pointerEvents="none">
                       <TextInput
-                        onChangeText={handleChange("dateOfBirth")}
-                        onBlur={handleBlur("dateOfBirth")}
                         placeholder="Date of birth..."
                         pointerEvents="none"
                         label="Date of birth"
-                        value={values.dateOfBirth}
+                        value={
+                          values.dateOfBirth
+                            ? dayjs(values.dateOfBirth).format("DD-MM-YYYY")
+                            : ""
+                        }
                         caption={errors.dateOfBirth}
                       />
                     </Pressable>
@@ -97,17 +115,19 @@ export const HomeScreen = observer(function HomeScreen() {
                     isVisible={isDatePickerVisible}
                     mode="date"
                     minimumDate={new Date(1930, 1, 1)}
-                    maximumDate={
-                      new Date(currentYear - 12, currentMonth, currentDay)
-                    }
                     onConfirm={(date) => {
-                      const _date = dayjs(date).format("DD-MM-YYYY");
-                      values.dateOfBirth = _date;
+                      const _date = dayjs(date).format("YYYY-MM-DD");
+                      handleChange("dateOfBirth")(_date);
                       setDatePickerVisibility(false);
                     }}
                     onCancel={() => {
                       setDatePickerVisibility(false);
                     }}
+                    date={
+                      values.dateOfBirth
+                        ? new Date(values.dateOfBirth)
+                        : undefined
+                    }
                   />
                 </View>
 
