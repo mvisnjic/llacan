3c87bf0deeab587c2bd772f025100acadc8db8a0
enrico <kokotenrico@gmail.com>
2022-05-23 08:47:00 +0200

    Return Person MST Model to Default Value

--- src/mobx/entities/person/Person.tsx
+++ src/mobx/entities/person/Person.tsx
@@ -4,14 +4,16 @@ export interface PersonInstance extends Instance<typeof Person> {}
 export interface PersonSnapshotIn extends SnapshotIn<typeof Person> {}
 export interface PersonSnapshotOut extends SnapshotOut<typeof Person> {}
 
-const { string, identifier, boolean, array } = types;
+const { string, identifier } = types;
 
 export const Person = types.model("Person", {
-  id: identifier,
-  title: string,
-  phone: string,
-  address: string,
-  sms_accept: boolean,
-  hasPommes: boolean,
-  tags: array(string),
+  url: identifier,
+  name: string,
+  height: string,
+  mass: string,
+  hair_color: string,
+  skin_color: string,
+  eye_color: string,
+  birth_year: string,
+  gender: string,
 });
