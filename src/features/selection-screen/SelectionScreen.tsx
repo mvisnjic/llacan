348d53fbd52dcd9b42b814e73b85159d48239135
enrico <kokotenrico@gmail.com>
2022-05-31 13:27:19 +0200

    Refactor Selection Screen

--- src/features/selection-screen/SelectionScreen.tsx
+++ src/features/selection-screen/SelectionScreen.tsx
@@ -48,11 +48,153 @@ function useStyle() {
   });
 }
 
-export const SelectionScreen = observer(function SelectionScreen() {
+export const Header = observer(function Header(props: { restaurant: any }) {
   const S = useStyle();
+  const { restaurant } = props;
+
+  return (
+    <View style={S.imageContainer}>
+      <Image
+        source={require("~/assets/street-fast-food-hamburger-with-bbq-grilled-steak-PE2NUQB1.png")}
+        style={S.Image}
+      />
+      <View style={S.textContainer}>
+        <Text sizeExtraLarge weightBold colorLight>
+          {restaurant.title}
+        </Text>
+        <Spacer />
+        <View flexDirectionRow alignItemsCenter>
+          <Icon
+            size={C.fontSizeMediumLarge}
+            name="home-outline"
+            style={S.IconColor}
+          />
+          <Spacer />
+          <Text sizeMediumSmall weightLight colorLight>
+            {restaurant.address}
+          </Text>
+        </View>
+        <Spacer />
+        <View flexDirectionRow alignItemsCenter>
+          <Icon
+            size={C.fontSizeMediumLarge}
+            name="phone-outline"
+            style={S.IconColor}
+          />
+          <Spacer />
+          <Text sizeMediumSmall weightLight colorLight>
+            {formatPhoneNumber(restaurant.phone)}
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+});
+
+export const MySelection = observer(function MySelection(props: {
+  restaurant: any;
+  orderInCart: any;
+  navigation: any;
+}) {
+  const S = useStyle();
+  const { orderInCart, navigation, restaurant } = props;
+
+  return (
+    <View
+      paddingLarge
+      flexDirectionRow
+      alignItemsCenter
+      style={S.mySelectionContainer}
+    >
+      <Text sizeMediumLarge weightSemiBold>
+        Moj odabir
+      </Text>
+      <Button
+        title="Odaberi za sebe"
+        disabled={!!orderInCart}
+        onPress={() =>
+          navigation.navigate("RestaurantMenuScreen", {
+            restaurant: restaurant,
+          })
+        }
+      />
+    </View>
+  );
+});
+
+export const UserOrder = observer(function UserOrder(props: {
+  orderInCart: any;
+  setOrderInCart: any;
+}) {
+  const S = useStyle();
+  const store = useStore();
+  const { orderInCart, setOrderInCart } = props;
+
+  return (
+    <View
+      style={{
+        borderBottomWidth: 1,
+        borderBottomColor: "#EEEEEE",
+        display: "flex",
+      }}
+      paddingMedium
+      flexDirectionRow
+    >
+      <View centerContent style={{ width: 70 }}>
+        <View style={S.circle} />
+        <Text sizeMedium weightSemiBold ellipsizeMode="tail" numberOfLines={1}>
+          User
+        </Text>
+      </View>
+      <Spacer />
+      <View>
+        <View flexDirectionRow>
+          <Text sizeMediumLarge weightSemiBold style={{ width: 220 }}>
+            {orderInCart.name}
+          </Text>
+          <View centerContent>
+            <Text sizeMediumLarge alignCenter>
+              {orderInCart.price},00 kn
+            </Text>
+          </View>
+        </View>
+        <View flexDirectionRow>
+          <Text sizeMediumSmall style={{ width: 220 }}>
+            {store.condimentStore.selectedCondimentsAsStrings.join(", ")}
+          </Text>
+          <IconButton
+            iconName="trash-can-outline"
+            iconColor={C.colorTextDanger}
+            centerContent
+            style={{ flex: 1 }}
+            onPress={() => setOrderInCart(undefined)}
+          />
+        </View>
+      </View>
+    </View>
+  );
+});
+
+export const OtherUserOrderCount = observer(function OtherUserOrderCount() {
+  const S = useStyle();
+
+  return (
+    <View
+      paddingLarge
+      flexDirectionRow
+      alignItemsCenter
+      style={S.mySelectionContainer}
+    >
+      <Text sizeMediumLarge weightSemiBold>
+        Ostali(0)
+      </Text>
+    </View>
+  );
+});
+
+export const SelectionScreen = observer(function SelectionScreen() {
   const navigation = useNavigation();
   const route = useRoute();
-  const store = useStore();
 
   const { restaurant, order } = route.params as {
     restaurant: typeof restaurantData[0];
@@ -63,120 +205,17 @@ export const SelectionScreen = observer(function SelectionScreen() {
 
   return (
     <Screen preventScroll>
-      <View>
-        <View style={S.imageContainer}>
-          <Image
-            source={require("~/assets/street-fast-food-hamburger-with-bbq-grilled-steak-PE2NUQB1.png")}
-            style={S.Image}
-          />
-          <View style={S.textContainer}>
-            <Text sizeExtraLarge weightBold colorLight>
-              {restaurant.title}
-            </Text>
-            <Spacer />
-            <View flexDirectionRow alignItemsCenter>
-              <Icon
-                size={C.fontSizeMediumLarge}
-                name="home-outline"
-                style={S.IconColor}
-              />
-              <Spacer />
-              <Text sizeMediumSmall weightLight colorLight>
-                {restaurant.address}
-              </Text>
-            </View>
-            <Spacer />
-            <View flexDirectionRow alignItemsCenter>
-              <Icon
-                size={C.fontSizeMediumLarge}
-                name="phone-outline"
-                style={S.IconColor}
-              />
-              <Spacer />
-              <Text sizeMediumSmall weightLight colorLight>
-                {formatPhoneNumber(restaurant.phone)}
-              </Text>
-            </View>
-          </View>
-        </View>
-        <View
-          paddingLarge
-          flexDirectionRow
-          alignItemsCenter
-          style={S.mySelectionContainer}
-        >
-          <Text sizeMediumLarge weightSemiBold>
-            Moj odabir
-          </Text>
-          <Button
-            title="Odaberi za sebe"
-            disabled={!!orderInCart}
-            onPress={() =>
-              navigation.navigate("RestaurantMenuScreen", {
-                restaurant: restaurant,
-              })
-            }
-          />
-        </View>
-        {orderInCart && (
-          <View
-            style={{
-              borderBottomWidth: 1,
-              borderBottomColor: "#EEEEEE",
-              display: "flex",
-            }}
-            paddingMedium
-            flexDirectionRow
-          >
-            <View centerContent style={{ width: 70 }}>
-              <View style={S.circle} />
-              <Text
-                sizeMedium
-                weightSemiBold
-                ellipsizeMode="tail"
-                numberOfLines={1}
-              >
-                User
-              </Text>
-            </View>
-            <Spacer />
-            <View>
-              <View flexDirectionRow>
-                <Text sizeMediumLarge weightSemiBold style={{ width: 220 }}>
-                  {orderInCart.name}
-                </Text>
-                <View centerContent>
-                  <Text sizeMediumLarge alignCenter>
-                    {orderInCart.price},00 kn
-                  </Text>
-                </View>
-              </View>
-              <View flexDirectionRow>
-                <Text sizeMediumSmall style={{ width: 220 }}>
-                  {store.condimentStore.selectedCondimentsAsStrings.join(", ")}
-                </Text>
-                <IconButton
-                  iconName="trash-can-outline"
-                  iconColor={C.colorTextDanger}
-                  centerContent
-                  style={{ flex: 1 }}
-                  onPress={() => setOrderInCart(undefined)}
-                />
-              </View>
-            </View>
-          </View>
-        )}
-        <View
-          paddingLarge
-          flexDirectionRow
-          alignItemsCenter
-          style={S.mySelectionContainer}
-        >
-          <Text sizeMediumLarge weightSemiBold>
-            Ostali(0)
-          </Text>
-        </View>
-      </View>
+      <Header restaurant={restaurant} />
+      <MySelection
+        orderInCart={orderInCart}
+        navigation={navigation}
+        restaurant={restaurant}
+      />
+      {orderInCart && (
+        <UserOrder orderInCart={orderInCart} setOrderInCart={setOrderInCart} />
+      )}
+      <OtherUserOrderCount />
+      {/* TODO lista narudzbi ostalih usera */}
     </Screen>
   );
 });
